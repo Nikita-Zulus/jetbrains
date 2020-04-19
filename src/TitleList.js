665fcb9a5b87c284efa2aebbea1b687fbefe3722
Никита <nikita-zulus@mail.ru>
2020-04-20 00:40:52 +0300

    serching and control improving

--- src/TitleList.js
+++ src/TitleList.js
@@ -1,77 +1,108 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
-export default function TitleList({ topLevelIds, pages, anchors }) {
+export default function TitleList({ topLevelIds, pages, anchors, name }) {
   return (
     <ul className="TitleList">
       {topLevelIds
         .map((id) => pages[id])
+        .filter((x) => (name === "" ? x : x.title === name))
         .map((page) => (
-          <TitleItem page={page} pages={pages} anchors={anchors} />
+          <TitleItem page={page} pages={pages} anchors={anchors} name={name} />
         ))}
     </ul>
   );
 }
 
-function TitleItem({ page, pages, anchors }) {
+function TitleItem({ page, pages, anchors, name }) {
   const [collapsed, setCollapsed] = useState(true);
   const [selectedId, setSelectedId] = useState(null);
+  const [slectedTarget, setSelectedTarget] = useState(null);
+  const [colored, setColored] = useState(null);
 
-  console.log(selectedId);
-  function toggleCollapsed() {
+  function toggleCollapsed(event) {
+    if (event.target === slectedTarget) {
+      return;
+    }
     setCollapsed((prev) => !prev);
   }
-
+  function handleColor(x) {
+    setColored(x);
+  }
   return (
-    <li key={page.id} id={page.id} className="TitleList__item">
-      <div
-        className="TitleAndAnchor"
-        style={
-          page.id === selectedId
-            ? { backgroundColor: "#f0f0f0" }
-            : { backgroundColor: "#fafafa" }
-        }
-        onClick={toggleCollapsed}
-        onMouseOver={() => setSelectedId(page.id)}
-        onMouseOut={() => setSelectedId(null)}
-      >
-        <div className="TitleList__header" tabindex="0">
-          {page.pages && (
-            <div
+    <Router>
+      <li key={page.id} id={page.id} className="TitleList__item">
+        <div
+          className="TitleAndAnchor"
+          style={
+            page.id === selectedId
+              ? {
+                  backgroundColor: "#f0f0f0",
+                  paddingLeft: `${page.level * 15}px`,
+                }
+              : {
+                  backgroundColor: "#fafafa",
+                  paddingLeft: `${page.level * 15}px`,
+                }
+          }
+          onClick={toggleCollapsed}
+          onMouseOver={() => setSelectedId(page.id)}
+          onMouseOut={() => setSelectedId(null)}
+        >
+          <div className="TitleList__header">
+            {page.pages && (
+              <div
+                className={
+                  collapsed
+                    ? "TitleList__CheckSqure__Rotate"
+                    : "TitleList__CheckSqure"
+                }
+              ></div>
+            )}
+            <Link
+              to={page.url}
               className={
-                collapsed
-                  ? "TitleList__CheckSqure__Rotate"
-                  : "TitleList__CheckSqure"
+                page.id === colored && !collapsed
+                  ? "TitleOnlyBold"
+                  : "TitleOnly"
               }
-            ></div>
-          )}
+              style={
+                page.pages === undefined
+                  ? { marginLeft: "15.5px" }
+                  : { marginLeft: "0px" }
+              }
+              onClick={() => handleColor(page.id)}
+            >
+              {page.title}
+            </Link>
+          </div>
           <div
-            className="TitleOnly"
-            style={
-              page.pages === undefined
-                ? { marginLeft: "15px" }
-                : { marginLeft: "0px" }
-            }
+            className="anchors"
+            onMouseOver={(e) => setSelectedTarget(e.target)}
+            onMouseOut={() => setSelectedTarget(null)}
           >
-            {page.title}
+            {!collapsed &&
+              page.anchors &&
+              page.anchors.map((anchor) => (
+                <Link
+                  to={anchors[anchor].url + anchors[anchor].anchor}
+                  className="Anchor"
+                  style={{ paddingLeft: "40px" }}
+                >
+                  {anchors[anchor].title}
+                </Link>
+              ))}
           </div>
         </div>
-        <div className="anchors">
-          {!collapsed &&
-            page.anchors &&
-            page.anchors.map((anchor) => (
-              <a
-                href={page.href + anchor}
-                className="Anchor"
-                style={{ paddingLeft: "15px" }}
-              >
-                {anchors[anchor].title}
-              </a>
-            ))}
-        </div>
-      </div>
-      {!collapsed && page.pages && (
-        <TitleList pages={pages} topLevelIds={page.pages} anchors={anchors} />
-      )}
-    </li>
+        {!collapsed && page.pages && (
+          <TitleList
+            pages={pages}
+            topLevelIds={page.pages}
+            anchors={anchors}
+            name={name}
+          />
+        )}
+      </li>
+    </Router>
   );
 }
